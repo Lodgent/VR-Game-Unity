fc913d9185e4246784eaff17456728d11490c009
Ramil Mavlyutov <stirk2281337@gmail.com>
2022-09-27 14:22:34 +0500

    Исправил баг: теперь коллайдер игрока двигается вместе с его камерой

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,7 +11,9 @@ public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
     private CharacterController characterController;
-    public float speed = 5;
+    public Transform playercent;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +25,6 @@ public class PlayerController : MonoBehaviour
     {
        Vector3 direction =  Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0,9.81f, 0) * Time.deltaTime);
+       characterController.center = new Vector3(playercent.localPosition.x, 1.03f, playercent.localPosition.z);
     }
 }

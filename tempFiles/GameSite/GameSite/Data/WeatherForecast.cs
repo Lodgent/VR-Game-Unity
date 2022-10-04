115335af2090e550a452f52fa7ae8a3e24cf66b3
Serkik <63666583+Serckik@users.noreply.github.com>
2022-10-05 00:38:33 +0500

    Добавил папку с кодом и зависимостями веб приложения

---
+++ tempFiles/GameSite/GameSite/Data/WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace GameSite.Data
+{
+    public class WeatherForecast
+    {
+        public static int a;
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ tempFiles/GameSite/GameSite/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+using GameSite.Data;
+namespace GameSite.Pages
+{
+    public partial class Counter
+    {
+        public static int currentCount = 0;
+
+        public static void IncrementCount()
+        {
+            WeatherForecast.a++;
+        }
+    }
+}
